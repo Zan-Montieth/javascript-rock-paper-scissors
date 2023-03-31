f8c09c106d8d2a5be508fc87de17d2f089449a81
Zan <zan.montieth@gmail.com>
2023-03-31 04:30:30 -0600

    setup string to int function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,15 +1,43 @@
+const ROCK = 0
+const PAPER = 1
+const SCISSORS = 2
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     switch(choice){
-        case 0:
+        case ROCK :
             return "rock"
-        case 1:
+        case PAPER:
             return "paper"
-        case 2:
+        case SCISSORS:
             return "scissors"
 
     }
-
 }
 
-console.log(getComputerChoice())
+function stringToNumber(stringIn){
+    switch(stringIn){
+        case "rock":
+            return 0;
+        case "paper":
+            return 1;
+        case "scissors":
+            return 2;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let normalizedPlayerSelection = playerSelection.toLowerCase()
+    // console.log(normalizedPlayerSelection);
+    // console.log(stringToNumber(normalizedPlayerSelection))
+    if(stringToNumber(normalizedPlayerSelection) == stringToNumber(computerSelection)){
+        return "Round is a Tie"
+    }else if(stringToNumber(normalizedPlayerSelection) == stringToNumber(computerSelection)){
+        
+    }
+}
+   
+
+const playerSelection = "PapeR";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
